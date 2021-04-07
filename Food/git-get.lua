f4a67dca5ec4d473ec81e337daf4c511f4ecd915
Dylan Arbour <arbourd@users.noreply.github.com>
2021-04-06 22:57:13 -0400

    git-get 0.2.4

--- Food/git-get.lua
+++ Food/git-get.lua
@@ -1,5 +1,5 @@
 local name = "git-get"
-local version = "0.2.3"
+local version = "0.2.4"
 
 food = {
     name = name,
@@ -12,7 +12,7 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://github.com/arbourd/git-get/releases/download/v" .. version .. "/" .. name .. "-v" .. version .. "-darwin-amd64.tar.gz",
-            sha256 = "01150150ee5fca4c9657e8850fe39fffabad33a47637a1c31b23dee421570dd2",
+            sha256 = "01de7b59878ec5beedc0b60228bfcd2db15d5cce5d30b1506006dea5614ff11d",
             resources = {
                 {
                     path = name,
@@ -25,7 +25,7 @@ food = {
             os = "linux",
             arch = "amd64",
             url = "https://github.com/arbourd/git-get/releases/download/v" .. version .. "/" .. name .. "-v" .. version .. "-linux-amd64.tar.gz",
-            sha256 = "c2fd8a8221d0a2799b5db0e78b30dd3bdad3b839ce0cb3395b3b604c02ac23d5",
+            sha256 = "3cc6af5b52fe7f9c63c8aa73e0a9cf4485541109fba9c64828ac71306872f91a",
             resources = {
                 {
                     path = name,
@@ -38,7 +38,7 @@ food = {
             os = "windows",
             arch = "amd64",
             url = "https://github.com/arbourd/git-get/releases/download/v" .. version .. "/" .. name .. "-v" .. version .. "-windows-amd64.zip",
-            sha256 = "b84794b7ce39f767372a252441c8c9de756f2daed725d6551073f974e4a34ebb",
+            sha256 = "c71a8b5b59e177bcda07f314d7cfb880d3a6fb6af33a59ddfaccd7af8d8a5fa4",
             resources = {
                 {
                     path = name .. ".exe",
