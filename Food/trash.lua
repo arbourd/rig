b53ad544cf680c528518d5d61cbf26d1742a2cf3
Dylan Arbour <arbourd@users.noreply.github.com>
2021-11-13 17:34:44 -0500

    trash 0.3.2

--- Food/trash.lua
+++ Food/trash.lua
@@ -1,5 +1,5 @@
 local name = "trash"
-local version = "0.3.1"
+local version = "0.3.2"
 
 food = {
     name = name,
@@ -12,7 +12,20 @@ food = {
             os = "darwin",
             arch = "amd64",
             url = "https://github.com/arbourd/trashOS/releases/download/v" .. version .. "/" .. name .. "-v" .. version .. "-darwin-amd64.tar.gz",
-            sha256 = "326ff8d50b8a00fc2ba191086e044df77e6623d10b9c9d8514f59ebb14d0f320",
+            sha256 = "bddbb8add6c1a291b573b1bd0bf38399cf2aba98c1d7566e01fbbc9814fe707c",
+            resources = {
+                {
+                    path = name,
+                    installpath = "bin/" .. name,
+                    executable = true
+                }
+            }
+        },
+        {
+            os = "darwin",
+            arch = "arm64",
+            url = "https://github.com/arbourd/trashOS/releases/download/v" .. version .. "/" .. name .. "-v" .. version .. "-darwin-arm64.tar.gz",
+            sha256 = "a1b1d20f959eeab9496769e20b1de49d81f98ed37cee6eb1f61f1b2ebc156501",
             resources = {
                 {
                     path = name,
